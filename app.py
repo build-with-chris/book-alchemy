48b9e2b2056a5ef7d6075df625647b9ac14d7f09
hecking_killa <chris.hermann9397@gmail.com>
2025-04-30 14:57:23 +0200

    Creating subclasses from the db

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ def dfrer():
     pass
 
 
+with app.app_context():
+    db.create_all()
 
-if __name__ =="__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
---
+++ data_models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, foreign, backref
+
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class Author(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column
+    birth_date: Mapped[int] = mapped_column
+    death_date: Mapped[int] = mapped_column
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    isbn: Mapped[str] = mapped_column (unique=True)
+    title: Mapped[str] = mapped_column
+    publication_year: Mapped[int] = mapped_column
+    author_id: Mapped[int] = db.relationship("Author", backref=backref("Author", uselist=False))
