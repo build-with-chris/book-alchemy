af0cd1ad9fad7a1450c35f4ebf29bbbc4a3cc662
hecking_killa <chris.hermann9397@gmail.com>
2025-04-30 16:16:48 +0200

    posting author and books functionality

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import redirect
@@ -9,10 +11,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.sqlite'
 
 db.init_app(app)
-@app.route("/author/<int:id>")
-def author_detail(id):
-    author = Author.query.get_or_404(id)
-    return render_template("author_detail.html", author=author)
+@app.route("/home")
+def home():
+    pass
 
 
 @app.route('/add_author', methods=['GET','POST'])
@@ -25,10 +26,24 @@ def add_author():
         )
         db.session.add(author)
         db.session.commit()
-        #return redirect(url_for("author_detail", id=author_id))
+        return f'Successfully added this author'
     return render_template("add_author.html")
 
-with app.app_context():
-    db.create_all()
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        book = Book(
+            isbn =request.form['isbn'],
+            title=request.form['title'],
+            publication_year=request.form['publication_year'],
+            author_id=request.form['author_id']
+        )
+        db.session.add(book)
+        db.session.commit()
+        return f"Successfully saved {book.title}."
+    authors = Author.query.all()
+    return render_template("add_book.html", authors=authors)
+
 
 app.run(port=5002, debug=True)
