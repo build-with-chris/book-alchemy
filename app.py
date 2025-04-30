ce860147ed279f0dc67bd418a6195be87281dc25
hecking_killa <chris.hermann9397@gmail.com>
2025-04-30 14:39:49 +0200

    initial setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+
+db.init_app(app)
+
+@app.route('/')
+def dfrer():
+    pass
+
+
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
